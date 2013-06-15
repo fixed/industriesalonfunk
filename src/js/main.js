594d417d12cc77d5c86ebfa2edcd6e2220876aae
fixed <tokyo.tron@gmail.com>
2013-06-16 01:55:12 +0200

    Implemented Tuner with event emitting and visual indicator.

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,28 @@ require.config({
 	paths : {
 		'jquery' : 'lib/jquery/jquery-1.9.1.min',
 		'plugins' : 'lib/requirejs/plugins',
-        'eventEmitter' : 'lib/eventEmitter/eventEmitter'
+        'eventEmitter' : 'lib/eventEmitter/eventEmitter',
+        'underscore' : 'lib/underscore/underscore',
+        'util' : 'lib/util'
 	}
 });
 
 require(['plugins/domReady', 'tuner/index'],function(domReady, tuner){
 	domReady(function(){
-		
-		tuner.init();
+
+		$.getJSON('../config_test.json').success(function(data){
+			
+			tuner.init({
+				channels : data.channels
+			});
+
+			tuner.on('tune', function(value) {
+				console.log(value);
+			})
+
+		}).fail(function(error) {
+			alert('Could not load configuration')
+		});
 
 	});
 });

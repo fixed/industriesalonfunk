0d235022052617b5ec4661186001b56d4660260b
Florian Demmer <demmer.florian@googlemail.com>
2013-06-24 20:27:54 +0200

    added sample based AudioInterference effect

--- src/js/media/audio/audio.js
+++ src/js/media/audio/audio.js
@@ -1,27 +1,29 @@
 define([
-	'media/base'
-	, 'util/class'
-	, 'media/audio/sample'
-	, 'media/audio/effect/oldRadio'
-	, 'media/audio/effect/distortion'
-	, 'media/audio/effect/whiteNoise'
-	, 'mixer/mixer'
-],function(Media, Class, Sample, OldRadio, Distortion, WhiteNoise, mixer){
+	'media/base',
+	'util/class',
+	'media/audio/sample',
+	'media/audio/effect/oldRadio',
+	'media/audio/effect/distortion',
+	'media/audio/effect/whiteNoise',
+	'media/audio/effect/interference',
+	'mixer/mixer'
+],function(Media, Class, Sample, OldRadio, Distortion, WhiteNoise, Interference, mixer){
 
 	var effects = {
-		old : OldRadio
-		, distortion : Distortion
-		, whitenoise : WhiteNoise
+		old : OldRadio,
+		distortion : Distortion,
+		whitenoise : WhiteNoise,
+		interference : Interference
 	};
 
-	function AudioMedia(options){
+	function AudioMedia(options) {
 		Media.call(this);
 
 		this._sample = new Sample(options.file);
 		this._effects = [];
 		this._output = this._sample; // main output
 
-		options.filters.forEach(function(properties){
+		options.filters.forEach(function(properties) {
 
 			var effect = this._createEffect(properties);
 			this._output.connect(effect.input);
@@ -30,30 +32,32 @@ define([
 			
 		}, this);
 
-	};
+	}
+
 	Class.inherits(AudioMedia,Media);
 
-	AudioMedia.prototype._createEffect = function(properties){
+	AudioMedia.prototype._createEffect = function(properties) {
 		var effectClass;
-		if(effects[properties.name]){
+
+		if (effects[properties.name]) {
 			effectClass = effects[properties.name];
-		} else{
+		} else {
 			throw new Error('Unknown audio effect: ' + properties.name);
 		}
 
-		return new effectClass({min : properties.min, max : properties.max });
+		return new effectClass({ min : properties.min, max : properties.max });
 	};
 
-	AudioMedia.prototype.load = function(){
+	AudioMedia.prototype.load = function() {
 		mixer.audio.attachSource(this._output);
 	};
 
-	AudioMedia.prototype.unload = function(){
+	AudioMedia.prototype.unload = function() {
 		mixer.audio.detachSource(this._output);
 	};
 
-	AudioMedia.prototype.tune = function(distVal){
-		this._effects.forEach(function(effect){
+	AudioMedia.prototype.tune = function(distVal) {
+		this._effects.forEach(function(effect) {
 			effect.tune(distVal);
 		});
 	};
---
+++ src/js/media/audio/effect/interference.js
@@ -0,0 +1,55 @@
+define([
+    'media/audio/effect/base',
+    'util/class',
+    'mixer/mixer'
+],function(Effect, Class, mixer) {
+
+	function Interference(options) {
+		Effect.call(this,options);
+
+		this.wet = mixer.audio.context.createGain();
+		this.interferenceSoure = mixer.audio.context.createBufferSource();
+
+		this.input.connect(this.output);
+		this.interferenceSoure.connect(this.wet);
+		this.wet.connect(this.output);
+
+		if (!Interference.audioBuffer) {
+
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', '/media/_statics/46728__freqman__radio-interfer.wav', true);
+			xhr.responseType = 'arraybuffer';
+
+			var that = this;
+			xhr.onload = function() {
+				mixer.audio.context.decodeAudioData(xhr.response, that._onAudioDecode.bind(that), that._onAudioDecodeError.bind(that));
+			};
+
+			xhr.send();
+		} else {
+			this.interferenceSoure.buffer = Interference.audioBuffer;
+			this.interferenceSoure.loop = true;
+			this.interferenceSoure.start(0);
+		}
+	}
+
+	Class.inherits(Interference,Effect);
+
+	Interference.prototype._onAudioDecode = function(buffer) {
+		if (!buffer) throw new Error('Could not decode Interference buffer');
+		Interference.audioBuffer = buffer;
+		this.interferenceSoure.buffer = Interference.audioBuffer;
+		this.interferenceSoure.loop = true;
+		this.interferenceSoure.start(0);
+	};
+
+	Interference.prototype._onAudioDecodeError = function(e) {
+		throw e;
+	};
+
+	Interference.prototype.setWetLevel = function(val){
+		this.wet.gain.value = val;
+	};
+
+	return Interference;
+});

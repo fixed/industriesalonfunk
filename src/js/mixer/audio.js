306a2916e968eb3d4e5c4b2e19ba94f64f231b42
Florian Demmer <demmer.florian@googlemail.com>
2013-06-24 23:23:59 +0200

    added static noise implementation to the AudioMixer. Each file gets loaded and it's volume is controlled (scaled by 0.4) by a sine-oscillator with a random frequency between 0 and 1.

--- src/js/mixer/audio.js
+++ src/js/mixer/audio.js
@@ -13,6 +13,51 @@ define([
 		this.staticsGain.connect(this.masterGain);
 		this.masterGain.connect(this.compressor);
 		this.compressor.connect(this.context.destination);
+
+		// load statics
+		statics.files.forEach(function(url) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', '/media/_statics/' + url, true);
+			xhr.responseType = 'arraybuffer';
+			xhr.mixer = this;
+			xhr.onload = function() {
+				this.mixer._addStaticAudio(this.response);
+			};
+			xhr.send();
+		}, this);
+	}
+
+	AudioMixer.prototype._addStaticAudio = function(buffer) {
+		this.context.decodeAudioData(buffer, this._onAudioDecode.bind(this), this._onAudioDecodeError.bind(this));
+
+	};
+
+	AudioMixer.prototype._onAudioDecode = function(buffer) {
+		if (!buffer) throw new Error('Could not decode AudioMixer statics audio');
+		
+		var player = this.context.createBufferSource();
+		player.buffer = buffer;
+		player.loop = true;
+
+		var osc = this.context.createOscillator();
+		osc.frequency.value = Math.random();
+
+		var oscGain = this.context.createGain();
+		oscGain.gain.value = 0.4;
+
+		var playerGain = this.context.createGain();
+
+		osc.connect(oscGain);
+		oscGain.connect(playerGain.gain);
+		player.connect(playerGain);
+		playerGain.connect(this.staticsGain);
+
+		osc.start(0);
+		player.start(0);
+	};
+
+	AudioMixer.prototype._onAudioDecodeError = function(e) {
+		throw e;
 	};
 
 	AudioMixer.prototype.attachSource = function(source){
